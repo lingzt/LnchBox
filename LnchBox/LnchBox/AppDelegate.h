8912d61dbb648800402e342dd39c3891cffd68b6
Ling <asklingtoby@gmail.com>
2016-05-25 10:08:06 -0400

    set up project LnchBox

---
+++ LnchBox/LnchBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LnchBox
+//
+//  Created by ling toby on 5/25/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
